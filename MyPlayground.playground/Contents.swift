b4fa989dae7b499887784d83b3d9fefb9fed732c
kirillmsc <kirillmsc@gmail.com>
2019-02-27 03:24:20 +0300

    Delete .DS_Store

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+var a : Int? = 1
+var b : Int? = 2
+var c : Int? = 3
+a != 0
+
+print ("Решить квадратное уравнение: ax^2 + bx + c = 0")
+print ("a не равно 0;")
+print("Найти корни уравнения.")
+
+var d = b!*b! - 4*a!*c!
+//print(d)
+//func (quad(a, b, c))
