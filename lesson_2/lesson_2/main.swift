f14200e4a6af0d63c4ed37d0c8ce93e343442ae2
Kirill <kirillmsc@gmail.com>
2019-03-01 01:59:51 +0300

    Add Lesson_2

---
+++ lesson_2/lesson_2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  lesson_2
+//
+//  Created by k.kochemasov on 01/03/2019.
+//  Copyright © 2019 k.kochemasov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
